a1ca6cb27402669bf64bb75a861adf9c5bc3e9f6
Garv Pandey <pandey.garv25@gmail.com>
2025-03-07 17:08:12 +0530

    set up font and basic css

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+
+export function Portfolio() {
+  return (
+    <>
+      <Header title="Portfolio" />
+
+      <Footer />
+    </>
+  )
+}
